3271edfc7c9be812fde607bc8fed06f6347944d9
Rhyzen16 <102942367+Rhyzen16@users.noreply.github.com>
2023-04-08 18:36:47 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,58 @@
+function validateForm() {
+
+  const username = document.getElementById("username").value;
+
+  const password = document.getElementById("password").value;
+
+  if (username === "" || password === "") {
+
+    document.getElementById("error-message").innerHTML = "Please fill out both fields.";
+
+    return false;
+
+  }
+
+  return true;
+
+}
+
+function loginUser() {
+
+  const username = document.getElementById("username").value;
+
+  const password = document.getElementById("password").value;
+
+  // Here, you would typically make an API call to verify the credentials
+
+  // In this example, we're just checking if the username and password match
+
+  if (username === "admin" && password === "password") {
+
+    alert("Login successful!");
+
+  } else {
+
+    alert("Invalid username or password.");
+
+  }
+
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    if (validateForm()) {
+
+      loginUser();
+
+    }
+
+  });
+
+});
+
